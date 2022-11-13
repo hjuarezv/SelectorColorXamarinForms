e5c2ef44670cbc19f752b9941d0b8c8929a398e5
hjuarezv <hjuarezx@gmail.com>
2022-11-12 20:52:04 -0500

    Proyecto de Seleccion de Color con Sliders

---
+++ SelectorColor/SelectorColor/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SelectorColor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MostrarColor(object sender, ValueChangedEventArgs e)
+        {
+            var valorRojo = (int)sliderRojo.Value;
+            var valorVerde = (int)sliderVerde.Value;
+            var valorAzul = (int)sliderAzul.Value;
+
+            String strColor = String.Format("#{0:X2}{1:X2}{2:X2}", (int)valorRojo, (int)valorVerde, (int)valorAzul);
+            
+            boxColor.BackgroundColor = Color.FromHex(strColor);
+
+            lblColor.Text = "Color en Hex: " + strColor;
+        }
+    }
+}
